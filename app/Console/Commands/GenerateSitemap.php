35020785ca9db8cf03969da8357224d74aaa1c1d
fajarkh <fajarkh9@gmail.com>
2026-09-13 20:38:39 +0800

    feat: Add console command to generate sitemap and schedule daily execution

---
+++ app/Console/Commands/GenerateSitemap.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Attributes\Description;
+use Illuminate\Console\Attributes\Signature;
+use Illuminate\Console\Command;
+use Spatie\Sitemap\SitemapGenerator;
+
+#[Signature('app:generate-sitemap')]
+#[Description('Generate Sitemap')]
+class GenerateSitemap extends Command
+{
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        SitemapGenerator::create(config('app.url'))
+            ->writeToFile(public_path('sitemap.xml'));
+    }
+}
